e1aaa1f98503772805bf7c9fb44a31825118d500
UT01228 <UT01228@UNICOMTIC25>
2024-10-11 14:18:27 +0530

    Task 2 : 2. Create a Course class

--- CourseManagementSystem_v1/Course .cs
+++ CourseManagementSystem_v1/Course .cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace CourseManagementSystem_v1
 {
-   
- 
+    public class Course
+    {
+
+     
+        public int CourseId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Duration { get; set; }
+
+        public decimal Price { get; set; }
+
+
+       
+    }
+
+    
 
 }
